bff2b13511e49667f2c67b42bde945316808641d
Daniel Becker <dbecker4130@gmail.com>
2016-12-22 08:39:40 -0800

    model schema complete

---
+++ model/artist.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const atristSchema = Schema({
+  name: { type: String, required: true},
+  timestamp: { type: Date, requied: true}
+});
+
+module.exports = mongoose.model('artist', artistSchema);
--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const morgan = require('morgan');
 const cors = require('cors');
 const Promise = require('bluebird');
 const mongoose = require('mongoose');
-const debug = require('debug')('list:server');
+const debug = require('debug')('artist:server');
 
 const app = express();
 const PORT = 3000;
+
 
 app.listen(PORT, () => {
   debug(`server up: ${PORT}`);
